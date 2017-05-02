32ebf9ad6602c0b6725c24f8ad7ff7d6e20fcb6d
Matthew Biesen <upandcomming88@gmail.com>
2017-05-02 00:11:00 -0500

    Update python_serial.py

--- pi_microbit_communication/python_serial.py
+++ pi_microbit_communication/python_serial.py
@@ -1,1 +1,15 @@
 # This file goes on the pc you are connecting to the microbit.  Only tested on raspberry pi.
+import serial
+import time
+
+PORT = "/dev/ttyACM0"
+BAUD = 115200
+s = serial.Serial(PORT)
+s.baudrate = BAUD
+s.parity = serial.PARITY_NONE
+s.databits = serial.EIGHTBITS
+s.stopbits = serial.STOPBITS_ONE
+
+while True:
+    s.write("hello".encode('UTF-8'))
+    time.sleep(5)
