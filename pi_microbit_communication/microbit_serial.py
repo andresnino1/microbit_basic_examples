6a31be0278788c1da855899f2dd7f9fc1e86a71a
Matthew Biesen <upandcomming88@gmail.com>
2017-05-02 00:16:34 -0500

    Update microbit_serial.py

--- pi_microbit_communication/microbit_serial.py
+++ pi_microbit_communication/microbit_serial.py
@@ -1,1 +1,12 @@
 # This is the code to be placed on the microbit
+from microbit import *
+
+while True:
+  sleep(500)
+  try:
+    bytestring = uart.readline()
+    receivedstring = str(bytestring)
+    display.scroll(receivedstring)
+  except:
+    pass
+  
