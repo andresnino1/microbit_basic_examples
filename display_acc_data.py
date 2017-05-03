6bd261a175d5f91232bfec377b6f4b222e8c2d8c
Matthew Biesen <upandcomming88@gmail.com>
2017-05-03 01:49:39 -0500

    Update display_acc_data.py

--- display_acc_data.py
+++ display_acc_data.py
@@ -1,16 +1,14 @@
 from microbit import *
 
 
-radio.on()
-
 def display_acc_data():
     my_send = accelerometer.get_values()               
     display.scroll("x")
-    display.scroll(my_send[0])
+    display.scroll(str(my_send[0]))
     display.scroll("y")
-    display.scroll(mysend[1])
+    display.scroll(str(mysend[1]))
     display.scroll("z")
-    display.scroll(mysend[2])
+    display.scroll(str(mysend[2]))
     sleep(800)
     
 while True:
