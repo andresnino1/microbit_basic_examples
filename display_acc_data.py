3f779da6367eec9338dfa1cdf777417ef8b0f1ce
Matthew Biesen <upandcomming88@gmail.com>
2017-05-03 01:50:51 -0500

    Update display_acc_data.py

--- display_acc_data.py
+++ display_acc_data.py
@@ -6,9 +6,9 @@ def display_acc_data():
     display.scroll("x")
     display.scroll(str(my_send[0]))
     display.scroll("y")
-    display.scroll(str(mysend[1]))
+    display.scroll(str(my_send[1]))
     display.scroll("z")
-    display.scroll(str(mysend[2]))
+    display.scroll(str(my_send[2]))
     sleep(800)
     
 while True:
